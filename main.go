3279a98f8b351fc587434fd8d160040a0817c12d
Nafas Ebrahimi <ebranafas@gmail.com>
2026-09-23 09:38:36 +0330

    define get and set methods

--- main.go
+++ main.go
@@ -6,12 +6,18 @@ type Store struct {
 	data map[string]string
 }
 
+func (s *Store) Get(key string) (string, bool) {
+	return "", false
+}
+
+func (s *Store) Set(key string, value string) {}
+
 func NewStore() *Store {
 	return &Store{
 		data: make(map[string]string)
 	}
 }
 
-func main() {
+func main() {	
     fmt.Println("Hello, World!")
 }
