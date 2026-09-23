564ff4ff4ba2dc4faf0a9a8197cc3a4fac4ea67d
Nafas Ebrahimi <ebranafas@gmail.com>
2026-09-23 21:01:55 +0330

    feat: add delete function

--- main.go
+++ main.go
@@ -15,6 +15,10 @@ func (s *Store) Set(key, value string) {
 	s.data[key] = value;
 }
 
+func (s *Store) Delete(key string) {
+	delete(s.data, key);
+}
+
 func NewStore() *Store {
 	return &Store{
 		data: make(map[string]string),
@@ -27,5 +31,9 @@ func main() {
 	data.Set("a", "nafas")
 	data.Set("b", "blah blah blah")
 
+	data.Delete("b")
+
 	fmt.Println(data.Get("a"))
+
+	fmt.Println(data.Get("b"))
 }
