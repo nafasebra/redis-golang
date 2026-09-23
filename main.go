ff2a10b017bcd2b4af76500a46d66c1b1d602fda
Nafas Ebrahimi <ebranafas@gmail.com>
2026-09-23 09:48:58 +0330

    working on get and set methods and test something

--- main.go
+++ main.go
@@ -7,17 +7,31 @@ type Store struct {
 }
 
 func (s *Store) Get(key string) (string, bool) {
-	return "", false
+	value, exist := s.data[key];
+	return value, exist
 }
 
-func (s *Store) Set(key string, value string) {}
+func (s *Store) Set(key, value string) {
+	s.data[key] = value;
+}
 
 func NewStore() *Store {
 	return &Store{
-		data: make(map[string]string)
+		data: make(map[string]string),
 	}
 }
 
 func main() {	
+	data := make(map[string]string)
+
+	data["a"] = "nafas"
+	data["b"] = "1234"
+
+	value, exists := data["b"];
+
+	fmt.Println(data["a"])
+	fmt.Println(value, exists)
+
+
     fmt.Println("Hello, World!")
 }
