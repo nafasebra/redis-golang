95f746a8f104dd0211ad51064208b50d65f5f1ad
Nafas Ebrahimi <ebranafas@gmail.com>
2026-09-24 22:21:53 +0330

    rename to store file

--- main.go
+++ main.go
@@ -1,44 +1,6 @@
 package main
 
-import {
-	"fmt",
-	"sort"
-}
-
-type Store struct {
-	data map[string]string
-}
-
-func (s *Store) Get(key string) (string, bool) {
-	value, exist := s.data[key];
-	return value, exist
-}
-
-func (s *Store) Set(key, value string) {
-	s.data[key] = value;
-}
-
-func (s *Store) Delete(key string) {
-	delete(s.data, key);
-}
-
-func (s *Store) Keys() ([]string) {
-	keys := make([]string, 0, len(s.data))
-
-	for key := range s.data {
-		keys = append(keys, key)
-	}
-
-	sort.Strings((keys))
-
-	return keys
-}
-
-func NewStore() *Store {
-	return &Store{
-		data: make(map[string]string),
-	}
-}
+import "fmt"
 
 func main() {	
 	data := NewStore()
